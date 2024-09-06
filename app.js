b3fbdb598487f17f3c6a8796b3118fb491bd4569
joshuadorley <joshuaboateng33@gmail.com>
2024-09-05 20:19:24 -0400

    Final work

--- app.js
+++ app.js
@@ -6,14 +6,33 @@ console.log(
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
-// YOUR CODE HERE
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+
 
 // Exercise 2
-console.log("EXERCISE 2:\n==========\n");
 
-// YOUR CODE HERE
+num + str = '1111'
+num + str2 = '11eleven'
+num + str2 = 12
+firstName + num = 'Frodo11'
+isPresent + str = 'true11'
+firstName + lastName = 'FrodoBaggins'
+
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
-// YOUR CODE HERE
+val == str3 = true;
+val === str3 = false;
+!isPresent2 = true;
+(“eleven” == str4 && val >= str3) = false;
+(!isPresent2 || isPresent2) = true;
+0 == false = true;
+0 === false = false;
+0 != false = false;
+0 !== false = true;
